74fdd9240ec736fc2cd2140277198b24224252d0
Guillem-M-A <guillem.martinez@institutvidreres.cat>
2025-03-26 13:37:49 +0100

    comanda sequalize auto

--- Server.js
+++ Server.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'patata',
-    database: 'ORMGuillemMartinez'
+    database: 'projecteahiramguilleene'
 });
 const app = require('express')();
 
@@ -32,12 +32,9 @@ const {crearConfigBaseDades} = require('app/db.config.js');
 const db = crearConfigBaseDades();
 const {getModelCity} = require('./app/models/city.model');
 
-const City = getModelCity(db);
-const {getModelCountry} = require('./app/models/country.model');
-const Country = getModelCountry(db);
 const initmodels = require('models/init-models');
 const {Sequelize} = require("sequelize");
-const {dept, doctor, empleat, hospital, malalt, plantilla, sala} = initmodels(db);
+const {usuari, productos, datos_de_compra} = initmodels(db);
 
 
 db.sync().then(() => {
